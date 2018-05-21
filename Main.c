68a9690936a457da30bf82859ee293255723b202
bryanclark90 <bryan.clark@email.wsu.edu>
2018-05-20 21:58:00 -0700

    Mounted file system with return messages

--- Main.c
+++ Main.c
@@ -6,4 +6,14 @@ int main(void)
     printf("Welcome, to my linux shell!\n");
 
     linux_shell_init();
+
+    if(fs_mount_root() == FAILURE)
+    {
+        printf("FAILURE: Mounting failed.\n");
+        exit(1);
+    }
+    else
+    {
+        printf("Connection Established!\n");
+    }
 }
